c2991f4b9536308ffea7631e4baec343aa5ef1c8
Alan Tan <alantanlc@gmail.com>
2022-03-27 21:32:29 +0800

    feat: update master worker

--- src/main.py
+++ src/main.py
@@ -1,24 +1,21 @@
 from mapreduce.wordcount.WordCountMapWorker import WordCountMapWorker
 from mapreduce.wordcount.WordCountReduceWorker import WordCountReduceWorker
-import os
+from mapreduce.Master import Master
 
 if __name__ == '__main__':
+  # Init parameters
   storage_dir = '../storage'
+  output_dir = '../output'
   input_filenames = ['input-1.txt', 'input-2.txt', 'input-3.txt', 'input-4.txt', 'input-5.txt']
   intermediate_filenames = ['intermediate-1.txt','intermediate-2.txt','intermediate-3.txt','intermediate-4.txt','intermediate-5.txt']
-  keys = ['apple', 'banana', 'cranberry', 'elderberry', 'fig']
-  map_workers = []
-  reduce_workers = []
+  output_filename = 'output.txt'
+  keys = ['apple', 'banana', 'cranberry', 'durian', 'elderberry', 'fig']
+  map_workers = [WordCountMapWorker() for i in range(len(input_filenames))]
+  reduce_workers = [WordCountReduceWorker() for i in range(len(keys))]
 
-  for i, j in zip(input_filenames, intermediate_filenames):
-    m = WordCountMapWorker()
-    m.assign(storage_dir, i, j)
-    m.execute()
+  # Master worker
+  master = Master(map_workers, reduce_workers, storage_dir, input_filenames, intermediate_filenames, output_filename, keys)
+  master.assign_map_workers()
+  master.assign_reduce_workers()
+  master.run()
 
-  with open(os.path.join(storage_dir, 'output', 'output.txt'), 'w') as f:
-    pass
-
-  for key in keys:
-    r = WordCountReduceWorker()
-    r.assign(storage_dir, intermediate_filenames, 'output.txt', key)
-    r.execute()
--- src/mapreduce/Master.py
+++ src/mapreduce/Master.py
@@ -1,24 +1,36 @@
+import os
+
 class Master:
 
-  def __init__(self, map_workers, reduce_workers, input_files, output_file):
+  def __init__(self, map_workers, reduce_workers, storage_dir, input_files, intermediate_files, output_file, keys):
     self.map_workers = map_workers
     self.reduce_workers = reduce_workers
+    self.storage_dir = storage_dir
+    self.output_dir = 'output'
     self.input_files = input_files
+    self.intermediate_files = intermediate_files
     self.output_file = output_file
-    self.assign_map(self.map_workers, self.input_files)
-    self.assign_reduce(self.reduce_workers, self.output_file)
+    self.keys = keys
 
-  def assign_map(self, workers, input_files):
+  def assign_map_workers(self):
     # For now let's assume that num of workers and input_files are the same
-    for worker, file_name in zip(workers, input_files):
-      worker.assign(file_name)
+    for worker, input_file, intermediate_file in zip(self.map_workers, self.input_files, self.intermediate_files):
+      worker.assign(self.storage_dir, input_file, intermediate_file)
 
-  def assign_reduce(self, workers, output_file):
+  def assign_reduce_workers(self):
+    # Assign each reduce worker with a key
     # All reduce workers will write to the same output file
-    for worker in workers:
-      worker.assign(output_file)
+    for worker, key in zip(self.reduce_workers, self.keys):
+      worker.assign(self.storage_dir, self.intermediate_files, self.output_file, key)
+
+  def clear_output(self):
+    with open(os.path.join(self.storage_dir, 'output', self.output_file), 'w') as f:
+      pass
 
   def run(self):
+    # Clear output file
+    self.clear_output()
+
     # Map
     for worker in self.map_workers:
       worker.execute()
